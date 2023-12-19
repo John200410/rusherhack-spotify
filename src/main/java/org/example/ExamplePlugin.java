c5842a81a8350f2f410fff57b19c884e11206635
John <43681932+John200410@users.noreply.github.com>
2023-12-19 12:59:39 -0500

    Initial commit

---
+++ src/main/java/org/example/ExamplePlugin.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import org.rusherhack.client.api.RusherHackAPI;
+import org.rusherhack.client.api.plugin.Plugin;
+
+/**
+ * Example rusherhack plugin
+ *
+ * @author John200410
+ */
+public class ExamplePlugin extends Plugin {
+	
+	@Override
+	public void onLoad() {
+		
+		//logger
+		this.getLogger().info("Hello World!");
+		
+		//creating and registering a new module
+		final ExampleModule exampleModule = new ExampleModule();
+		RusherHackAPI.getModuleManager().registerFeature(exampleModule);
+		
+		//creating and registering a new hud element
+		final ExampleHudElement exampleHudElement = new ExampleHudElement();
+		RusherHackAPI.getHudManager().registerFeature(exampleHudElement);
+		
+		//creating and registering a new command
+		final ExampleCommand exampleCommand = new ExampleCommand();
+		RusherHackAPI.getCommandManager().registerFeature(exampleCommand);
+	}
+	
+	@Override
+	public void onUnload() {
+		this.getLogger().info("Example plugin unloaded!");
+	}
+	
+	@Override
+	public String getName() {
+		return "Example";
+	}
+	
+	@Override
+	public String getVersion() {
+		return "v1.0";
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Example rusherhack plugin";
+	}
+	
+	@Override
+	public String[] getAuthors() {
+		return new String[]{"John200410"};
+	}
+}
